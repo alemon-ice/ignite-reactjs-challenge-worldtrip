6841c3858db83d98cb4dffd05eb292a17821d8e1
Rafael Dias <rafaeldiasdesouza.rd@gmail.com>
2022-01-26 08:08:18 -0300

    Finalize slider

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Divider, Flex } from '@chakra-ui/react';
+import { Box, Divider, Flex, Image, Text } from '@chakra-ui/react';
 import Head from 'next/head';
 import { Banner } from '../components/Banner';
 import { Slider } from '../components/Slider';
@@ -15,29 +15,38 @@ export default function Home() {
 
         <Flex marginY="20" justify="space-evenly">
           <Box width={158} align="center">
-            <img src="/images/Nightlife.png" alt="vida noturna" />
+            <Image src="/images/Nightlife.png" alt="vida noturna" />
           </Box>
           <Box width={158} align="center">
-            <img src="/images/Beach.png" alt="praia" />
+            <Image src="/images/Beach.png" alt="praia" />
           </Box>
           <Box width={158} align="center">
-            <img src="/images/Modern.png" alt="moderno" />
+            <Image src="/images/Modern.png" alt="moderno" />
           </Box>
           <Box width={158} align="center">
-            <img src="/images/Classic.png" alt="clássico" />
+            <Image src="/images/Classic.png" alt="clássico" />
           </Box>
           <Box width={158} align="center">
-            <img src="/images/More.png" alt="e mais..." />
+            <Image src="/images/More.png" alt="e mais..." />
           </Box>
         </Flex>
 
         <Divider
-          marginBottom="13"
           width="20"
           height="0.5"
           backgroundColor="gray.600"
           alignSelf="center"
         />
+
+        <Text
+          fontWeight="medium"
+          fontSize="3xl"
+          textAlign="center"
+          marginY="14"
+        >
+          Vamos nessa? <br />
+          Então escolha seu continente
+        </Text>
 
         <Slider />
       </Flex>
